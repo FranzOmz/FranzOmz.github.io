ba962b197d985075557b9b2274548ad2a0abdee6
FranzOmz <kyspac@gmail.com>
2016-03-30 18:17:37 +0300

    Added simple movements to goalkeepers

--- js/main.js
+++ js/main.js
@@ -4,12 +4,38 @@ var leftKeeper = document.getElementById('goalkeeper_left'),
 rightKeeper = document.getElementById('goalkeeper_right');
 
 function goalKeeperMover(event) {
+	var value = 20,
+		leftStartValue = leftKeeper.offsetTop,
+		rightStartValue = rightKeeper.offsetTop;
+
 	if (event.which === 119) {
-		let value = 20,
-		// startValue = leftKeeper.getComputedStyle();
-		console.log(leftKeeper[0].clientWidth);
-		//leftKeeper.style.top = startValue + value + 'px';
-		//console.log(leftKeeper.style.top = startValue + value + 'px')
+		if (leftKeeper.offsetTop < 20) {
+			return false;
+		}
+
+		leftKeeper.style.top = leftStartValue - value + 'px';
+	}
+
+	if (event.which === 115) {
+		if (leftKeeper.offsetTop > 460) {
+			return false;
+		}
+		leftKeeper.style.top = leftStartValue + value + 'px';
+	}
+
+	if (event.which === 105) {
+		if (rightKeeper.offsetTop < 20) {
+			return false;
+		}
+
+		rightKeeper.style.top = rightStartValue - value + 'px';
+	}
+
+	if (event.which === 107) {
+		if (rightKeeper.offsetTop > 460) {
+			return false;
+		}
+		rightKeeper.style.top = rightStartValue + value + 'px';
 	}
 }
 
