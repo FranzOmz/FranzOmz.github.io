4caf036d42abfc5595e42278c7193797a23e4741
FranzOmz <kyspac@gmail.com>
2016-03-31 14:06:16 +0300

    Added russian keys, images, score, tutorial

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var leftKeeper = document.getElementById('goalkeeper_left'),
-rightKeeper = document.getElementById('goalkeeper_right');
+	rightKeeper = document.getElementById('goalkeeper_right');
 
 function goalKeeperMover(event) {
 	var value = 20,
 		leftStartValue = leftKeeper.offsetTop,
 		rightStartValue = rightKeeper.offsetTop;
-
-	if (event.which === 119) {
+		console.log(event)
+	if (event.which === 119 || event.which === 1094) {
 		if (leftKeeper.offsetTop < 20) {
 			return false;
 		}
@@ -16,14 +16,14 @@ function goalKeeperMover(event) {
 		leftKeeper.style.top = leftStartValue - value + 'px';
 	}
 
-	if (event.which === 115) {
+	if (event.which === 115 || event.which === 1099) {
 		if (leftKeeper.offsetTop > 460) {
 			return false;
 		}
 		leftKeeper.style.top = leftStartValue + value + 'px';
 	}
 
-	if (event.which === 105) {
+	if (event.which === 105 || event.which === 1096) {
 		if (rightKeeper.offsetTop < 20) {
 			return false;
 		}
@@ -31,12 +31,11 @@ function goalKeeperMover(event) {
 		rightKeeper.style.top = rightStartValue - value + 'px';
 	}
 
-	if (event.which === 107) {
+	if (event.which === 107 || event.which === 1083) {
 		if (rightKeeper.offsetTop > 460) {
 			return false;
 		}
 		rightKeeper.style.top = rightStartValue + value + 'px';
 	}
 }
-
 document.documentElement.addEventListener("keypress", goalKeeperMover);
